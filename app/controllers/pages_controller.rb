634534e3aeac4e740215c52b542681aaaed457a3
Anmol Khandeparkar <anmolkhandeparkar.nmims@gmail.com>
2016-06-14 23:11:34 +0530

    Implemented strong parameters and the profiles of individual users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,12 @@ class PagesController < ApplicationController
   end
   #back-end code for pages/profile view
   def profile
+    if (User.find_by_username(params[:id]))
+    @username = params[:id]
+  else
+    redirect_to root_path , :notice => "User not found ! "
   end
+end
   #back-end code for pages/explore view
   def explore
   end
