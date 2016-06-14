eea4aceeba61b9329259da279b8078c342843875
Anmol Khandeparkar <anmolkhandeparkar.nmims@gmail.com>
2016-06-14 13:40:04 +0530

    Created the Pages controller and their corresponding views of index profile explore and home

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  #back-end code for pages/index view
+  def index
+  end
+  #back-end code for pages/home view
+  def home
+  end
+  #back-end code for pages/profile view
+  def profile
+  end
+  #back-end code for pages/explore view
+  def explore
+  end
+end
