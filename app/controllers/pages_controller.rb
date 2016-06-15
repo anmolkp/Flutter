43c92c081b86b861b9d4f27c856816167af0a466
Anmol Khandeparkar <anmolkhandeparkar.nmims@gmail.com>
2016-06-15 11:29:38 +0530

    Added the creation and display of posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   end
   #back-end code for pages/home view
   def home
+      @posts = Post.all;
   end
   #back-end code for pages/profile view
   def profile
@@ -12,8 +13,11 @@ class PagesController < ApplicationController
   else
     redirect_to root_path , :notice => "User not found ! "
   end
+  @posts = Post.all.where("user_id=?",User.find_by_username(params[:id]).id);
+  @newPost = Post.new;
 end
   #back-end code for pages/explore view
   def explore
+      @posts = Post.all;
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts
   #Define Root URL
   root 'pages#index'
 
